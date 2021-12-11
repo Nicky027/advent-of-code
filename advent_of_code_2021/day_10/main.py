7cf2ef7a7554153626b9f479cb6356c65e90b08f
Nicky Abela <nicky@fasttrack-solutions.com>
2021-12-11 08:23:58 +0100

    2021 Day 10 - V2

--- advent_of_code_2021/day_10/main.py
+++ advent_of_code_2021/day_10/main.py
@@ -1,27 +1,30 @@
 import syntax as s
 import syntax_v2 as s2
+import timeit
 
 
-def main():
+def main(version=1):
     with open("input.txt", "r") as file:
         inputs = file.readlines()
 
     inputs = [input.replace("\n", "") for input in inputs]
 
     # version 1
-    errors, completions = s.find_errors_and_completions(inputs)
-    # part 1
-    print(s.score_errors(errors))
-    # part 2
-    print(s.score_completions(completions))
+    if version == 1:
+        errors, completions = s.find_errors_and_completions(inputs)
+        # part 1
+        print(s.score_errors(errors))
+        # part 2
+        print(s.score_completions(completions))
 
     # version 2
-    errors, completions = s2.find_errors_and_completions(inputs)
-    # part 1
-    print(s2.score_errors(errors))
-    # part 2
-    print(s2.score_completions(completions))
+    if version == 2:
+        errors, completions = s2.find_errors_and_completions(inputs)
+        # part 1
+        print(s2.score_errors(errors))
+        # part 2
+        print(s2.score_completions(completions))
 
 
 if __name__ == "__main__":
-    main()
+    main(version=1)
